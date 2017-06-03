a043d8ed03735757081c9530a75af33f60186dfd
Sergio Mota <sergioleomota12@gmail.com>
2017-06-02 23:52:35 -0400

    Uncomment a line of code

--- Web assignment 4/script.js
+++ Web assignment 4/script.js
@@ -1,4 +1,4 @@
-//document.body.style.backgroundColor = "red";
+document.body.style.backgroundColor = "red";
 document.getElementById("about").style.backgroundColor = "blue";
 document.getElementsByTagName("body")[0].style.fontFamily = "sans-serif";
 
